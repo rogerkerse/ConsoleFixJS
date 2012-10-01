ec7d789a59f01195360aa1eca85142b15faec9f3
unknown <roger@roger.webmedia.int>
2012-10-01 14:53:48 +0300

    IE console fix javascript.

---
+++ consoleFix.js
@@ -0,0 +1,4 @@
+/* IE console.log fix */
+/* Author: Roger Kerse */
+/* https://github.com/rogerkerse/ConsoleFixJS */
+if (typeof console === "undefined" || typeof console.log === "undefined") {console = {log: function() {}}

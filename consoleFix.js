e4ff608cdd95b83073a1676274c3546281b2dcd6
unknown <roger@roger.webmedia.int>
2012-10-02 09:11:28 +0300

    1. Comment style changed 2. README added

--- consoleFix.js
+++ consoleFix.js
@@ -1,4 +1,7 @@
-/* IE console.log fix */
-/* Author: Roger Kerse */
-/* https://github.com/rogerkerse/ConsoleFixJS */
+/**
+ * IE console.log fix
+ * 
+ * Author: Roger Kerse
+ * https://github.com/rogerkerse/ConsoleFixJS
+ */
 if (typeof console === "undefined" || typeof console.log === "undefined") {console = {log: function() {}}
